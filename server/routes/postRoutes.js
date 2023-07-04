5712d3c27391d83dc979f5c54c6099d56731bfd9
Yevhenii Danoi <geneforapp@gmail.com>
2023-07-04 14:57:48 +0300

    client-branch first commit

--- server/routes/postRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getPosts } = require("../controllers/postController");
-const router = express.Router();
-
-router.get("/", getPosts);
-
-module.exports = router;
