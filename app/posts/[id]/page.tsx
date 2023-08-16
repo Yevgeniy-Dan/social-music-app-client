56fbb2c53da8239cd9b0ec9e8911340dafb9e27e
Igor Stepanov <stepanovigor2110@gmail.com>
2023-08-16 13:04:07 +0300

    for deploy

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,56 +1,203 @@
-'use client'
+"use client";
+import { useMutation, useQuery } from "@apollo/client";
+
+import { useParams, useRouter } from "next/navigation";
+
+import { FormEventHandler, useEffect, useState } from "react";
+
+import { AnimatePresence, motion as m } from "framer-motion";
+
+import { useDispatch, useSelector } from "react-redux";
 
 import PostComment from "@/components/Comment/PostComment";
-import { useGetPostQuery } from "@/redux/api/postsApi";
-import Like from "@/ui/Like";
+import OurLike from "@/ui/OurLike";
 import OurInput from "@/ui/OurInput";
 import UserBage from "@/ui/UserBage";
-import { useParams } from "next/navigation";
-import { useState } from "react";
-import {AnimatePresence, motion as m} from "framer-motion";
 
-const page = () => {
+import { GET_POST } from "@/graphql/query/posts";
+import { CREATE_COMMENT } from "@/graphql/mutation/createComment";
+import { Comment, CommentResponse, MutationCreateCommentArgs } from "@/@types/graphql";
+import {
+  toggleShowAllComments,
+  selectComment,
+  clearReply,
+} from "@/redux/slices/commentSlice";
 
-  const [fullComments, setFullComments] = useState(false)
+import { AppDispatch } from "@/redux/store";
 
-  const toggleFullComments = () => setFullComments(prev => !prev)
+const PostPage = () => {
+  const [newComment, setNewComment] = useState("");
+  const { replyId, replyUser, showAllComments } = useSelector(selectComment);
+  const dispatch = useDispatch<AppDispatch>();
+  const params = useParams();
+  const navigate = useRouter();
 
-  const params = useParams()
+  useEffect(() => {
+    return () => {
+      dispatch(clearReply());
+    };
+  }, []);
 
+  const { data, loading } = useQuery(GET_POST, {
+    variables: { id: params.id },
+  });
+  const [addComment] = useMutation<CommentResponse, MutationCreateCommentArgs>(
+    CREATE_COMMENT,
+    {
+      refetchQueries: [
+        {
+          query: GET_POST,
+          variables: { id: params.id },
+        },
+      ],
+    }
+  );
 
-  const {data, isLoading} = useGetPostQuery(`${params.id}`)
+  interface TRecursionComment extends Comment { children: Comment[] }
 
-  if(data) {
-    console.log(data.post, params.id)
+  let com1: TRecursionComment[];
+
+  if (data?.post.comments) {
+    const recursion = (parentId: null | string): any => {
+      return data?.post.comments
+        .filter((c) => c.parentId === parentId)
+        .map((c) => ({ ...c, children: recursion(c.id) }));
+    };
+    com1 = recursion(null);
   }
-  if(isLoading) {
-    return 'Zagruzka...'
+
+  const addNewComment: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    addComment({
+      variables: {
+        createCommentInput: {
+          content: newComment,
+          postId: params.id,
+          parentId: replyId,
+        },
+      },
+    });
+    setNewComment("");
+    dispatch(clearReply());
+  };
+
+  if (loading) {
+    return (
+      <div className="bg-white rounded-[20px] flex flex-col p-5 min-h-[94vh]">
+        <div className="min-h-[480px] bg-gray-100 rounded-[15px]"></div>
+        <div className="my-4 flex items-center">
+          <div className="w-14 h-14 rounded-full bg-gray-100"></div>
+          <div className="flex-col ml-4 ">
+            <div className="w-[100px] h-4 bg-gray-100 mb-2 rounded-[8px]"></div>
+            <div className="w-[50px] h-4 bg-gray-100 rounded-[8px]"></div>
+          </div>
+          <div className="w-10 h-10 bg-gray-100 ml-auto rounded-[8px]"></div>
+          <div className="w-10 h-10 bg-gray-100 ml-4 rounded-[8px]"></div>
+        </div>
+        <div className="h-[1px] bg-[#f0f0f0]"></div>
+        <div className="my-4 flex items-center">
+          <div className="w-14 h-14 rounded-full bg-gray-100"></div>
+          <div className="flex-col ml-4 ">
+            <div className="w-[100px] h-4 bg-gray-100 mb-2 rounded-[8px]"></div>
+            <div className="w-[50px] h-4 bg-gray-100 rounded-[8px]"></div>
+          </div>
+          <div className="w-10 h-10 bg-gray-100 ml-auto rounded-[8px]"></div>
+          <div className="w-10 h-10 bg-gray-100 ml-4 rounded-[8px]"></div>
+        </div>
+        <div className="h-12 bg-gray-100 rounded-[8px]"></div>
+        <div className="my-4 flex items-center">
+          <div className="w-14 h-14 rounded-full bg-gray-100"></div>
+          <div className="flex-col ml-4 ">
+            <div className="w-[100px] h-4 bg-gray-100 mb-2 rounded-[8px]"></div>
+            <div className="w-[50px] h-4 bg-gray-100 rounded-[8px]"></div>
+          </div>
+          <div className="w-10 h-10 bg-gray-100 ml-auto rounded-[8px]"></div>
+          <div className="w-10 h-10 bg-gray-100 ml-4 rounded-[8px]"></div>
+        </div>
+        <div className="h-12 bg-gray-100 rounded-[8px]"></div>
+      </div>
+    );
   }
 
-  console.log(data)
-
-  const commentsCount = fullComments ? -1 : 2
+  const commentsCount = showAllComments ? com1.length : 2;
 
   return (
-    <div className={`bg-white rounded-[20px] flex flex-col p-5 ${fullComments ? '' : 'max-h-[94vh]'} relative`}>
+    <div
+      className={`bg-white rounded-[20px] flex flex-col p-5 ${
+        showAllComments ? "" : "max-h-[94vh]"
+      } relative`}>
       <div className="max-h-[480px] min-h-[300px] rounded-[15px] overflow-hidden flex items-center">
-        <img className="object-fill object-center" src={data?.post.mediaUrl} alt="" />
+        <img
+          className="w-full h-full object-fill object-center"
+          src={data?.post.mediaUrl}
+          alt=""
+        />
       </div>
       <div className="flex my-4">
-        <UserBage {...data?.post.user!}/>
-        <div className="flex gap-2 items-center mr-5"><span>{data?.post.totalComments}</span><img src="/Comment.svg" alt="Comment" /></div>
-        <Like count={data?.post.totalLikes}/>
+        <UserBage {...data?.post.user!} />
+        <div className="flex gap-2 items-center mr-5">
+          <span>{data?.post.totalComments}</span>
+          <img src="/Comment.svg" alt="Comment" />
+        </div>
+        <OurLike postId={params.id} />
       </div>
       <div className="h-[1px] bg-[#f0f0f0] mb-4"></div>
-      {
-        data?.post?.comments?.slice(0, commentsCount).map((comment, index) => {
-          return <PostComment key={comment.id} user={comment.user} content={comment.content}/>
-        })
-      }
-      <div className="h-[165px] bg-gradient-to-b from-transparent to-white absolute bottom-[76px] left-0 right-0 z-10 flex justify-center items-end pb-4"><button onClick={toggleFullComments} className="bg-white text-blueText font-semibold py-2 px-4 border border-blueText rounded-full">Show more</button></div>
-      <OurInput placeholder="Write your comment..."/>
+      <div className="overflow-hidden">
+        {com1 &&
+          com1.slice(0, commentsCount).map((comment, index) => {
+            return (
+              <PostComment
+                key={comment.id}
+                user={comment.user}
+                id={comment.id}
+                content={comment.content}
+                children={comment.children}
+              />
+            );
+          })}
+      </div>
+      {showAllComments ? (
+        <div className="absolute bottom-[56px] left-0 right-0 z-10 flex justify-center items-end">
+          <button
+            onClick={() => dispatch(toggleShowAllComments())}
+            className="text-blueText bg-white font-semibold py-2 px-4 underline">
+            Hide more
+          </button>
+        </div>
+      ) : (
+        <div className="h-[165px] bg-gradient-to-b from-transparent to-white absolute bottom-[75px] left-0 right-0 z-10 flex justify-center items-end pb-4">
+          <button
+            onClick={() => dispatch(toggleShowAllComments())}
+            className="bg-white text-blueText font-semibold py-2 px-4 border border-blueText rounded-full">
+            Show more
+          </button>
+        </div>
+      )}
+      <form className="relative" onSubmit={(e) => addNewComment(e)}>
+        <OurInput
+          placeholder="Write your comment..."
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+          submitIcon={"../Send.svg"}
+        />
+        {replyUser && (
+          <span className="absolute inline-block bottom-[-10px] px-2 bg-white left-5 text-blueText">
+            {`@${replyUser}, `}
+            <span
+              onClick={() => dispatch(clearReply())}
+              className="px-2 border border-blueText rounded-full">
+              x
+            </span>
+          </span>
+        )}
+      </form>
+      <button
+        onClick={() => navigate.back()}
+        className="bg-grad text-white py-2 px-3 absolute right-10 top-10 rounded-[10px]">
+        Back
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default PostPage;
--- components/Comment/PostComment.tsx
+++ components/Comment/PostComment.tsx
@@ -1,22 +1,47 @@
-import { Comment } from "@/@types/types"
-import UserBage from "@/ui/UserBage"
-import { FC } from "react"
+import { useDispatch } from "react-redux";
 
+import { setNameForReply } from "@/redux/slices/commentSlice";
+import { AppDispatch } from "@/redux/store";
+import UserBage from "@/ui/UserBage";
+import { Comment } from "@/@types/graphql";
 
-type TPostComment = Pick<Comment, 'user' | 'content'>
+const PostComment = ({ user, id, setReplyId, content, children }) => {
+  const dispatch = useDispatch<AppDispatch>();
 
-const PostComment: FC<TPostComment> = ({ user, content }) => {
+  const nested = children
+    ? children.map((comment: Comment, index: number) => {
+        return (
+          <PostComment
+            key={comment.id}
+            user={comment.user}
+            id={comment.id}
+            content={comment.content}
+            children={comment.children}
+          />
+        );
+      })
+    : null;
+
   return (
     <>
       <div>
-        <div className="flex">
+        <div className="flex items-center">
           <UserBage {...user} />
-          <div className="flex gap-2 items-center"><span>121</span><img src="/Heart.svg" alt="Heart" /></div>
+          <div
+            onClick={() =>
+              dispatch(
+                setNameForReply({ replyId: id, replyUser: user.username })
+              )
+            }
+            className="ml-4 text-blueText underline">
+            Reply
+          </div>
         </div>
         <p className="my-3 ml-2">{content}</p>
       </div>
+      <div className="ml-6">{nested}</div>
     </>
-  )
-}
+  );
+};
 
-export default PostComment
+export default PostComment;
