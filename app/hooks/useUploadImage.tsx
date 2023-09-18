8df0ae6c26f89152b25c41be545c92a6ece871f4
Igor Stepanov <stepanovigor2110@gmail.com>
2023-09-18 13:40:58 +0300

    add storage in .env

--- app/hooks/useUploadImage.tsx
+++ app/hooks/useUploadImage.tsx
@@ -1,52 +1,52 @@
-import { useMutation } from "@apollo/client";
-import axios from "axios";
-import { ChangeEventHandler, useEffect, useRef, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useMutation } from '@apollo/client'
+import axios from 'axios'
+import { ChangeEventHandler, useEffect, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { selectAuth, updateProfile } from "@/redux/slices/authSlice";
-import { UPDATE_PROFILE } from "@/graphql/mutation/updateUser";
-import { CREATE_POST } from "@/graphql/mutation/createPost";
+import { selectAuth, updateProfile } from '@/redux/slices/authSlice'
+import { UPDATE_PROFILE } from '@/graphql/mutation/updateUser'
+import { CREATE_POST } from '@/graphql/mutation/createPost'
 
-type TFile = "avatar" | "post"
+type TFile = 'avatar' | 'post'
 
 const useUploadImage = (typeFile: TFile) => {
-  const [uploadFile, setUploadFile] = useState<FormData | null>(null);
-  const [urlFile, setUrlFile] = useState<string | null>("");
-  const inputRef = useRef<HTMLInputElement>(null);
-  const { user } = useSelector(selectAuth);
-  const dispatch = useDispatch();
+  const [uploadFile, setUploadFile] = useState<FormData | null>(null)
+  const [urlFile, setUrlFile] = useState<string | null>('')
+  const inputRef = useRef<HTMLInputElement>(null)
+  const { user } = useSelector(selectAuth)
+  const dispatch = useDispatch()
 
-  const [addNewAvatar, { loading: avatarLoading }] = useMutation(UPDATE_PROFILE);
-  const [createPost, { loading: postLoading }] = useMutation(CREATE_POST);
+  const [addNewAvatar, { loading: avatarLoading }] = useMutation(UPDATE_PROFILE)
+  const [createPost, { loading: postLoading }] = useMutation(CREATE_POST)
 
   const storageApi =
-    typeFile === "avatar"
-      ? "http://localhost:5050/api/user/avatar/create"
-      : "http://localhost:5050/api/post/create";
+    typeFile === 'avatar'
+      ? process.env.STORAGE_AVATARS || 'http://localhost:5050/api/user/avatar/create'
+      : process.env.STORAGE_POSTS || 'http://localhost:5050/api/post/create'
 
   useEffect(() => {
     // if (user?.avatar) navigate.push('/')
-    if (!uploadFile) return;
+    if (!uploadFile) return
     const getMediaUrlAvatar = async () => {
       const res = await axios.post(storageApi, uploadFile, {
         headers: {
-          accept: "application/json",
-          "Accept-Language": "en-US,en;q=0.8",
-          "Content-Type": "multipart/form-data;",
+          accept: 'application/json',
+          'Accept-Language': 'en-US,en;q=0.8',
+          'Content-Type': 'multipart/form-data;',
         },
-      });
+      })
 
-      const mediaUrl = res.data.mediaUrl;
+      const mediaUrl = res.data.mediaUrl
 
-      setUrlFile(mediaUrl);
-      if (typeFile === "post") {
+      setUrlFile(mediaUrl)
+      if (typeFile === 'post') {
         createPost({
           variables: {
             createPostInput: {
               mediaUrl,
             },
           },
-        });
+        })
       } else {
         addNewAvatar({
           variables: {
@@ -60,35 +60,35 @@ const useUploadImage = (typeFile: TFile) => {
               updateProfile({
                 avatar: e.updateUser.avatar,
               })
-            );
+            )
           },
-        });
+        })
       }
-    };
+    }
 
-    getMediaUrlAvatar();
+    getMediaUrlAvatar()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [uploadFile]);
+  }, [uploadFile])
 
   const handleAvatar: ChangeEventHandler<HTMLInputElement> = (e) => {
     if (e.target.files) {
-      const data = new FormData();
-      data.append("media", e.target.files[0]);
-      setUploadFile(data);
+      const data = new FormData()
+      data.append('media', e.target.files[0])
+      setUploadFile(data)
     }
-  };
+  }
 
   const pickFile = () => {
-    inputRef.current?.click();
-  };
+    inputRef.current?.click()
+  }
 
   return {
     inputRef,
     pickFile,
     urlFile,
     handleAvatar,
-    loading: typeFile === "avatar" ? avatarLoading : postLoading,
-  };
-};
+    loading: typeFile === 'avatar' ? avatarLoading : postLoading,
+  }
+}
 
-export default useUploadImage;
+export default useUploadImage
