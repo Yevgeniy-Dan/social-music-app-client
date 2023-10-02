50e7aeca78384ad322e35526f038cc9ea411a052
Igor Stepanov <stepanovigor2110@gmail.com>
2023-10-02 11:23:43 +0300

    add env in codegen

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:8080/graphql',
+  schema:  process.env.GRAPHQL || 'http://localhost:8080/graphql',
   documents: ['./app/**/*.tsx', './graphql/**/*.ts', './app/**'],
   generates: {
     './@types/': {
